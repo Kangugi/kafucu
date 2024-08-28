c435f576518e9ca6486214e0c847caebd604f3cd
kangugi <muthama3897@gmail.com>
2024-08-28 08:09:54 +0300

    updates

--- index.php
+++ index.php
@@ -54,40 +54,13 @@
     <div class="container-fluid top-feature py-5 pt-lg-0">
         <div class="container py-5 pt-lg-0">
             <div class="row gx-0">
-                <div class="col-lg-4 wow fadeIn" data-wow-delay="0.1s">
-                    <div class="bg-white shadow d-flex align-items-center h-100 px-5" style="min-height: 160px;">
-                        <div class="d-flex">
-                            <div class="flex-shrink-0 btn-lg-square rounded-circle bg-light">
-                                <i class="fa fa-times text-primary"></i>
-                            </div>
-                            <div class="ps-3">
-                                <h4>Non-Denominational</h4>
-                                <span>Clita erat ipsum lorem sit sed stet duo justo</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
                 <div class="col-lg-4 wow fadeIn" data-wow-delay="0.3s">
                     <div class="bg-white shadow d-flex align-items-center h-100 px-5" style="min-height: 160px;">
                         <div class="d-flex">
                             <div class="flex-shrink-0 btn-lg-square rounded-circle bg-light">
                                 <i class="fa fa-users text-primary"></i>
                             </div>
-                            <div class="ps-3">
-                                <h4>Non-Political</h4>
-                                <span>Clita erat ipsum lorem sit sed stet duo justo</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
-                <div class="col-lg-4 wow fadeIn" data-wow-delay="0.3s">
-                    <div class="bg-white shadow d-flex align-items-center h-100 px-5" style="min-height: 160px;">
-                        <div class="d-flex">
-                            <div class="flex-shrink-0 btn-lg-square rounded-circle bg-light">
-                                <i class="fa fa-users text-primary"></i>
-                            </div>
                             <div class="ps-3">
                                 <h4>Voluntary</h4>
                                 <span>Clita erat ipsum lorem sit sed stet duo justo</span>
@@ -102,6 +75,7 @@
                             <div class="flex-shrink-0 btn-lg-square rounded-circle bg-light">
                                 <i class="fa fa-users text-primary"></i>
                             </div>
+
                             <div class="ps-3">
                                 <h4>Common-Faith</h4>
                                 <span>Clita erat ipsum lorem sit sed stet duo justo</span>
@@ -116,27 +90,17 @@
                             <div class="flex-shrink-0 btn-lg-square rounded-circle bg-light">
                                 <i class="fa fa-users text-primary"></i>
                             </div>
+
                             <div class="ps-3">
                                 <h4>Convictionaal</h4>
                                 <span>Clita erat ipsum lorem sit sed stet duo justo</span>
                             </div>
+
                         </div>
                     </div>
                 </div>
 
-                <div class="col-lg-4 wow fadeIn" data-wow-delay="0.5s">
-                    <div class="bg-white shadow d-flex align-items-center h-100 px-5" style="min-height: 160px;">
-                        <div class="d-flex">
-                            <div class="flex-shrink-0 btn-lg-square rounded-circle bg-light">
-                                <i class="fa fa-phone text-primary"></i>
-                            </div>
-                            <div class="ps-3">
-                                <h4>Non-Partisan</h4>
-                                <span>Clita erat ipsum lorem sit sed stet duo justo</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+               
             </div>
         </div>
     </div>
